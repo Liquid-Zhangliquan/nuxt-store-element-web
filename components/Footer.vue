bfa0cf4203fb80c51b8308571915a35fa95179a7
Liquid-Zhangliquan <1063754495@qq.com>
2019-03-28 10:18:49 +0800

    change footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
         <footer class="footer">
             <div class="content has-text-centered">
-                2019 ©武汉智图科技有限责任公司
+                2019 ©xx科技有限责任公司
             </div>
         </footer>
 </tenplate>
